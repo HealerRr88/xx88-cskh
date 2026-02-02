5abfc9fcd046008aa5abfe5aaa1a8a86044465ae
BeijingDev <beijingq8@gmail.com>
2026-02-02 18:20:46 +0700

    update TAI_KHOAN_URL in config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ export const BACK_HOME_URL = "https://rr88_shortlink.rr88tino.workers.dev/xx88li
 export const NAP_TIEN_URL = "https://secure.livechatinc.com/licence/19230099/v2/open_chat.cgi";
 export const RUT_TIEN_URL = "https://xx88.klivechat.com/widget"; //update 02-02-2026
 export const KHUYEN_MAI_URL = "https://secure.livechatinc.com/licence/19230099/v2/open_chat.cgi";
-export const TAI_KHOAN_URL = "https://secure.livechatinc.com/licence/19230099/v2/open_chat.cgi";
+export const TAI_KHOAN_URL = "https://xx88.klivechat.com/widget"; //update 02-02-2026
 export const KHIEU_NAI_URL = "https://t.me/XX88KHIEUNAI";
 export const TELEGRAM_URL = "https://t.me/BOTCSKHXX88_BOT";
 
