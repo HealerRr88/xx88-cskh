de02940b3f636ef8abd52f93582fc421c19ab5c6
BeijingDev <beijingq8@gmail.com>
2026-05-06 13:57:41 +0700

    remove default klivechat

--- src/utils/config.js
+++ src/utils/config.js
@@ -10,10 +10,10 @@ export const DEVICES = {
 
 export const BACK_HOME_URL = "https://rr88_shortlink.rr88tino.workers.dev/xx88link";
 
-export const NAP_TIEN_URL = "https://secure.livechatinc.com/licence/19230099/v2/open_chat.cgi";
-export const RUT_TIEN_URL = "https://secure.livechatinc.com/licence/19230099/v2/open_chat.cgi";
-export const KHUYEN_MAI_URL = "https://secure.livechatinc.com/licence/19230099/v2/open_chat.cgi";
-export const TAI_KHOAN_URL = "https://secure.livechatinc.com/licence/19230099/v2/open_chat.cgi"; //https://xx88.klivechat.com/widget
+export const NAP_TIEN_URL = "#";
+export const RUT_TIEN_URL = "#";
+export const KHUYEN_MAI_URL = "#";
+export const TAI_KHOAN_URL = "#"; //https://xx88.klivechat.com/widget
 export const KHIEU_NAI_URL = "https://t.me/XX88KHIEUNAI";
 export const TELEGRAM_URL = "https://t.me/BOTCSKHXX88_BOT";
 
